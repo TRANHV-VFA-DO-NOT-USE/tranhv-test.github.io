a4a158b5515ceb6ef9d5a5822378bb16cacd9e4f
tranhv <tranhv@vitalify.asia>
2021-01-21 16:43:28 +0700

    text

--- index.js
+++ index.js
@@ -9,3 +9,18 @@ function onClickCapture(){
 function onClickShareTwitter(){
 
 }
+
+var myInput = document.getElementById('myFileInput');
+
+function sendPic() {
+    var file = myInput.files[0];
+    console.log('senpic');
+    console.log(file)
+    document.getElementById("myImg").src = URL.createObjectURL(file);;
+
+    // Send file here either by adding it to a `FormData` object 
+    // and sending that via XHR, or by simply passing the file into 
+    // the `send` method of an XHR instance.
+}
+
+myInput.addEventListener('change', sendPic, false);
