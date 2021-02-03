01b9cf8a55e9084ec3479c52c6182b0ddc14538a
tranhv <tranhv@vitalify.asia>
2021-02-03 11:32:31 +0700

    text

--- index.js
+++ index.js
@@ -24,3 +24,17 @@ function sendPic() {
 }
 
 myInput.addEventListener('change', sendPic, false);
+
+const shareButton = document.querySelector('.share-button');
+
+shareButton.addEventListener('click', event => {
+  if (navigator.share) { 
+   navigator.share({
+      title: 'WebShare API Demo',
+      url: 'https://codepen.io/ayoisaiah/pen/YbNazJ'
+    }).then(() => {
+      console.log('Thanks for sharing!');
+    })
+    .catch(console.error);
+    }
+});
